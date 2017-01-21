8fccba99b344349b5eadb168ed660de604cec58c
alex <aleks.lecha@gmail.com>
2017-01-21 11:04:13 +0300

    Added validating input search string.

--- Hello/views.py
+++ Hello/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 import datetime
+from books.models import Book
 
 def hello(request):
     re = request.META['REMOTE_ADDR']
@@ -21,3 +22,18 @@ def hours_ahead(request, offset):
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
 
     return render(request, 'hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
+
+
+def search(request):
+    errors = []
+    if 'q' in request.GET:
+        q = request.GET['q']
+        if not q:
+            errors.append("Enter a search term")
+        elif len(q) > 20:
+            errors.append("Please enter at most 20 characters.")
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render(request, 'search_result.html', {'books': books, 'query': q})
+
+    return render(request, 'search_form.html', {'error': errors})
