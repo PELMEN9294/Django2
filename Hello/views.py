4360ee3a75659c9898223279515744010625e025
alex <aleks.lecha@gmail.com>
2017-01-21 09:05:55 +0300

    1

--- Hello/views.py
+++ Hello/views.py
@@ -16,7 +16,7 @@ def hours_ahead(request, offset):
     try:
         offset = int(offset)
     except ValueError:
-        raise Http404()
+        raise Http404()1
 
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
 
